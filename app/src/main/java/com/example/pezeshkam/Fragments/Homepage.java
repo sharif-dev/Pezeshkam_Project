78cb464d1a600215c63dd0d2cb7169c8c1c87b0a
AlirezaDizaji <alirezadizaji@yahoo.com>
2020-07-25 16:38:17 +0430

    Homepage Handlers and Threads added.

--- app/src/main/java/com/example/pezeshkam/Fragments/Homepage.java
+++ app/src/main/java/com/example/pezeshkam/Fragments/Homepage.java
@@ -1,26 +1,52 @@
 package com.example.pezeshkam.Fragments;
 
+import android.content.Context;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.pezeshkam.Adapters.HomepageAdapter;
+import com.example.pezeshkam.Models.DoctorCard;
 import com.example.pezeshkam.R;
 import com.google.android.material.textfield.TextInputEditText;
 
+import java.util.ArrayList;
+
 public class Homepage extends Fragment {
     TextInputEditText editText;
+    Context context;
+    ListView listView;
+    public Homepage(Context context) {
+        this.context = context;
+    }
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ArrayList<DoctorCard> doctorCards = new ArrayList<>();
 
-        return inflater.inflate(R.layout.homepage, container, false);
+        doctorCards.add(new DoctorCard("alireza", "09302377651", "sdf", "روانشناس"));
+        doctorCards.add(new DoctorCard("alireza", "09302377651", "sdf", "روانشناس"));
+        doctorCards.add(new DoctorCard("alireza", "09302377651", "sdf", "روانشناس"));
+        doctorCards.add(new DoctorCard("alireza", "09302377651", "sdf", "روانشناس"));
+        doctorCards.add(new DoctorCard("alireza", "09302377651", "sdf", "روانشناس"));
+        doctorCards.add(new DoctorCard("alireza", "09302377651", "sdf", "روانشناس"));
+        doctorCards.add(new DoctorCard("alireza", "09302377651", "sdf", "روانشناس"));
+        doctorCards.add(new DoctorCard("alireza", "09302377651", "sdf", "روانشناس"));
+        doctorCards.add(new DoctorCard("alireza", "09302377651", "sdf", "روانشناس"));
+        View view = inflater.inflate(R.layout.homepage, container, false);
+        listView = view.findViewById(R.id.list1);
+        ArrayAdapter<DoctorCard> adapter = new HomepageAdapter(this.context, 0, doctorCards);
+        listView.setAdapter(adapter);
+        return view;
     }
 }
---
+++ app/src/main/java/com/example/pezeshkam/TmpArrays.java
@@ -0,0 +1,32 @@
+package com.example.pezeshkam;
+
+import com.example.pezeshkam.Models.DoctorCard;
+
+import java.util.ArrayList;
+
+class TmpArrays {
+    private static TmpArrays tmpArrays = null;
+    private TmpArrays() {
+
+    }
+    public static TmpArrays getInstance()
+    {
+        if (tmpArrays == null)
+            tmpArrays = new TmpArrays();
+
+        return tmpArrays;
+    }
+    public ArrayList<DoctorCard> getDoctorCards() {
+        ArrayList<DoctorCard> doctorCards = new ArrayList<>();
+        doctorCards.add(new DoctorCard("alireza", "09302377651", "sdf", "روانشناس"));
+        doctorCards.add(new DoctorCard("alireza", "09302377651", "sdf", "روانشناس"));
+        doctorCards.add(new DoctorCard("alireza", "09302377651", "sdf", "روانشناس"));
+        doctorCards.add(new DoctorCard("alireza", "09302377651", "sdf", "روانشناس"));
+        doctorCards.add(new DoctorCard("alireza", "09302377651", "sdf", "روانشناس"));
+        doctorCards.add(new DoctorCard("alireza", "09302377651", "sdf", "روانشناس"));
+        doctorCards.add(new DoctorCard("alireza", "09302377651", "sdf", "روانشناس"));
+        doctorCards.add(new DoctorCard("alireza", "09302377651", "sdf", "روانشناس"));
+        doctorCards.add(new DoctorCard("alireza", "09302377651", "sdf", "روانشناس"));
+        return doctorCards;
+    }
+}
