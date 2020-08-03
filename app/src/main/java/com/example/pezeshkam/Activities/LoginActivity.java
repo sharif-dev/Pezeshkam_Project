9b8daac35f5a62e09b03190d9937bdd306ef2182
AmirHossein Karimi <amirhkarimi1999@gmail.com>
2020-08-03 14:44:43 +0430

    login done

--- app/src/main/java/com/example/pezeshkam/Activities/LoginActivity.java
+++ app/src/main/java/com/example/pezeshkam/Activities/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.pezeshkam.Activities;
 
+import android.opengl.Visibility;
 import android.os.Bundle;
 
 import com.example.pezeshkam.R;
@@ -13,6 +14,7 @@ import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ProgressBar;
 
 public class LoginActivity extends AppCompatActivity {
 
